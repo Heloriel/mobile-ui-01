74b682b7fe272b0c3be391edaaea3ef48f412cbe
Heloriel <andradematheus.dev@gmail.com>
2022-08-19 14:04:25 -0300

    Fix: open external icon on profile

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,4 +1,4 @@
-import { CaretLeft } from "phosphor-react";
+import { ArrowSquareOut, CaretLeft } from "phosphor-react";
 import Header from "../components/Header/Header";
 import ScreenContent from "../components/ScreenContent/ScreenContent";
 import UserProfile from "../components/UserProfile/UserProfile";
@@ -6,7 +6,7 @@ import UserProfile from "../components/UserProfile/UserProfile";
 export default function Profile() {
   return (
     <div className="canva">
-      <Header title="Profile" disableNavTabs leftIcon={<CaretLeft size={22} />} />
+      <Header title="Profile" disableNavTabs leftIcon={<CaretLeft size={22} />} rightIcon={<ArrowSquareOut size={22} />} />
       <UserProfile />
       <ScreenContent>aaaa</ScreenContent>
     </div>
