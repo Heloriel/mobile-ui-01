572e239d56bca514d321c76f46abca4b45b96dc3
Heloriel <andradematheus.dev@gmail.com>
2022-08-22 15:02:10 -0300

    Adjust Community layout

--- src/components/atoms/Community/Community.tsx
+++ src/components/atoms/Community/Community.tsx
@@ -5,8 +5,8 @@ type Props = {
 
 export default function Community({ id, name }: Props) {
   return (
-    <div className="flex rounded-lg overflow-hidden relative min-h-[150px] bg-red-500">
-      <img src={`/images/community/${id}.jpg`} className="object-cover" />
+    <div className="flex rounded-lg overflow-hidden relative min-h-[150px]">
+      <img src={`/images/community/${id}.jpg`} className="object-cover mx-auto lg:rounded-lg" />
       <span className="flex items-center absolute bottom-2 right-1/2 translate-x-1/2 text-white bg-project-primary rounded-full px-2">{name}</span>
     </div>
   );
