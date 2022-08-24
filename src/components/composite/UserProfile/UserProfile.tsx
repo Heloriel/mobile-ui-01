8b4fc33fcb0890c63b5766fab8048a9123ac1df1
Heloriel <andradematheus.dev@gmail.com>
2022-08-24 15:46:05 -0300

    Fix @ at profile page

--- src/components/composite/UserProfile/UserProfile.tsx
+++ src/components/composite/UserProfile/UserProfile.tsx
@@ -6,7 +6,7 @@ export default function UserProfile() {
     <div className="flex flex-col w-full items-center mb-8">
       <img src="https://github.com/Heloriel.png" className="rounded-2xl w-28 h-28 object-cover" />
       <span className="text-white text-2xl mt-4 font-bold">Matheus Andrade</span>
-      <SmallCatTitle title="@andradematheus" className="my-0 mb-6" />
+      <SmallCatTitle title="@andradematheus" className="flex w-full justify-center my-0 mb-6" />
       <UserStatus />
     </div>
   );
